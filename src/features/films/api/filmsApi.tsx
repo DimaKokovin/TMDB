3ca1e7b81addccfa65f1ea5bbd3ea3bfdd45f5d7
Dmitry <kokovin.02@inbox.ru>
2026-01-08 16:59:06 +0300

    first commit

--- src/features/films/api/filmsApi.tsx
+++ src/features/films/api/filmsApi.tsx
@@ -1,6 +1,6 @@
 import {baseApi} from "../../../app/baseApi.tsx";
 import type { FilmWithMoreDate, MovieCredits, PopularFilmsResponse} from "./filmsApi.type.ts";
-
+import {API_KEY} from "../../../../"
 
 export const filmsApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
@@ -9,7 +9,7 @@ export const filmsApi = baseApi.injectEndpoints({
         //         method: 'GET',
         //         url:'movie/popular',
         //         params:{
-        //             api_key:"8d518023a58ab3a9680eaafa735a4d71"
+        //            api_key:import.meta.env.API_KEY,
         //         }
         //     })  //картинка на главное
         // есть повторение useGetPopularFilmQuery и useGetPopularMovieQuery. стоит ли это исправлять?
@@ -22,7 +22,8 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:'search/movie',
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    // api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
                     query:searchTerm,
                     page
                 } // запрос на получение фильма по поиску
@@ -33,7 +34,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:'movie/popular',
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
                     page
 
                 }
@@ -44,7 +45,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:'movie/top_rated',
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
                     page
 
                 }
@@ -55,7 +56,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:'movie/upcoming',
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
                     page
                 }
             })
@@ -65,7 +66,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:'movie/now_playing',
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
                     page
 
                 }
@@ -76,7 +77,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:`movie/${movie_id}`,
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
 
 
                 }
@@ -87,7 +88,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:`movie/${movie_id}/credits`,
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
 
 
                 }
@@ -98,7 +99,7 @@ export const filmsApi = baseApi.injectEndpoints({
                 method: 'GET',
                 url:`movie/${movie_id}/similar`,
                 params:{
-                    api_key:"8d518023a58ab3a9680eaafa735a4d71",
+                    api_key:import.meta.env.API_KEY,
 
 
                 }
